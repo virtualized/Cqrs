23b860acbb02705912254bcd3f8d7967717bce39
James Bartram <james@v5.me.uk>
2016-03-21 23:04:20 +0000

    Add untracked items

---
+++ Cqrs.Connections/IConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cqrs.Connections
+{
+    public interface IConnectionFactory
+    {
+        IConnection CreateConnection();
+    }
+
+    public interface IConnection : IDisposable
+    {
+        void Open();
+        int Execute(string sql, object param = null);
+        IEnumerable<TResult> Query<TResult>(string sql, object param = null);
+        Task<IEnumerable<TResult>> QueryAsync<TResult>(string sql, object param = null);
+    }
+
+    public interface IConnectionStringProvider
+    {
+        string ConnectionString { get; }
+    }
+
+    public class ConnectionStringProvider : IConnectionStringProvider
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Cqrs/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Cqrs.Application.Domain;
+using Cqrs.Application.Queries;
+using Cqrs.Querying;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Cqrs.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IAsyncQueryHandler<FindUsersBySearchTextQuery, IEnumerable<User>> asyncHandler;
+        private readonly IQueryHandler<FindUsersBySearchTextQuery, IEnumerable<User>> handler;
+
+        public UserController(
+            IQueryHandler<FindUsersBySearchTextQuery, IEnumerable<User>> handler,
+            IAsyncQueryHandler<FindUsersBySearchTextQuery, IEnumerable<User>> asyncHandler)
+        {
+            this.handler = handler;
+            this.asyncHandler = asyncHandler;
+        }
+
+        [Route("search/{searchText}")]
+        [HttpGet]
+        public ActionResult Search(string searchText)
+        {
+            var query = new FindUsersBySearchTextQuery
+            {
+                SearchText = searchText,
+                IncludeInactiveUsers = true
+            };
+            return View(handler.Handle(query));
+        }
+
+        [Route("searchasync/{searchText}")]
+        [HttpGet]
+        public async Task<ActionResult> SearchAsync(string searchText)
+        {
+            var query = new FindUsersBySearchTextQuery { SearchText = searchText, IncludeInactiveUsers = true };
+            var users = await asyncHandler.Handle(query);
+            return View(users);
+        }
+    }
+}
